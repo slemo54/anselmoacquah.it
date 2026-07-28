433e4613a5c92bfa9cd0d478cabdd8b75232d430
slemo54 <77224018+slemo54@users.noreply.github.com>
2026-07-28 17:02:58 +0200

    config: redirect root to portfolio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Redirect root to portfolio */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/portfolio/",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
